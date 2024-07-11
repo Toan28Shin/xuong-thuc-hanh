703605489791ded4e5be7e7a6f7eaacbfcf76f4f
Toan28Shin <toannqph49664@gmail.com>
2024-07-11 10:30:45 +0700

    KhoiTao
    des

---
+++ GiaoDien/src/swing/MenuItem.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
+ */
+package swing;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import model.Model_Menu;
+
+/**
+ *
+ * @author tkun2
+ */
+public class MenuItem extends javax.swing.JPanel {
+
+    private boolean selected;
+    
+    public MenuItem(Model_Menu data) {
+        initComponents();
+        setOpaque(false);
+        if (data.getType() == Model_Menu.MenuType.MENU) {
+            lbIcon.setIcon(data.toIcon());
+            lbName.setText(data.getName());
+        } else if(data.getType() == Model_Menu.MenuType.TITLE){
+            lbIcon.setText(data.getName());
+            lbIcon.setFont(new Font("sansserif", 1, 12));
+            lbName.setVisible(false);
+        } else{
+            lbName.setText(" ");
+        }
+    }
+
+     public boolean isSelected() {
+        return selected;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+        repaint();
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        lbIcon = new javax.swing.JLabel();
+        lbName = new javax.swing.JLabel();
+
+        lbIcon.setFont(new java.awt.Font("SansSerif", 0, 12)); // NOI18N
+        lbIcon.setForeground(new java.awt.Color(255, 255, 255));
+
+        lbName.setFont(new java.awt.Font("SansSerif", 0, 14)); // NOI18N
+        lbName.setForeground(new java.awt.Color(255, 255, 255));
+        lbName.setText("Menu Name");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(lbIcon)
+                .addGap(18, 18, 18)
+                .addComponent(lbName)
+                .addContainerGap(7, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(lbName, javax.swing.GroupLayout.DEFAULT_SIZE, 50, Short.MAX_VALUE)
+            .addComponent(lbIcon, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+    }// </editor-fold>//GEN-END:initComponents
+    
+    @Override
+    protected void paintComponent(Graphics grphcs) {
+        if(selected) {
+            Graphics2D g2 = (Graphics2D) grphcs;
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2.setColor(new Color(255, 255, 255, 80));
+            g2.fillRoundRect(0, 0, getWidth(), getHeight(), 15, 15);
+        }
+        super.paintComponent(grphcs); 
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel lbIcon;
+    private javax.swing.JLabel lbName;
+    // End of variables declaration//GEN-END:variables
+}
